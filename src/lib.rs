f22bf0007dab02e1691b730007a93f215ccc9d85
Fredrik Broman <fredrik.broman@me.com>
2022-12-07 16:24:48 +0100

    No need for new() to be pub

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 
-pub fn new() -> HashMap<usize, char> {
+fn new() -> HashMap<usize, char> {
     let base45_charset = vec![
         '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
         'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
