43c042a70d1c983590c8d5a97af0c9d787bc773c
Fredrik Broman <fredrik.broman@conoa.se>
2023-05-05 15:30:18 +0200

    Remove copy paste code

--- src/lib.rs
+++ src/lib.rs
@@ -24,17 +24,8 @@ fn new_encode_lut() -> HashMap<usize, char> {
 }
 
 fn new_decode_lut() -> HashMap<char, usize> {
-    let base45_charset = vec![
-        '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
-        'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-        ' ', '$', '%', '*', '+', '-', '.', '/', ':',
-    ];
-
-    let mut base45map = HashMap::new();
-    for (i, x) in base45_charset.iter().enumerate() {
-        base45map.insert(x.clone(), i);
-    }
-    base45map
+    let base45_charset = new_encode_lut();
+    return base45_charset.iter().map(|(k, v)| (v.clone(), k.clone())).collect();
 }
 
 pub fn b45encode(v: Vec<u8>) -> String {
